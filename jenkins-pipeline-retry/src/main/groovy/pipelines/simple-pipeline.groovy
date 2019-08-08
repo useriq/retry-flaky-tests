9f7f2177309ece55aece978e0c94ac85897b0d8e
mikhailovav <mikhailovav@yamoney.ru>
2019-08-08 15:12:16 +0300

    add pipeline

--- jenkins-pipeline-retry/src/main/groovy/pipelines/simple-pipeline.groovy
+++ jenkins-pipeline-retry/src/main/groovy/pipelines/simple-pipeline.groovy
@@ -1,2 +1,18 @@
 package main.groovy.pipelines
 
+pipeline {
+    agent {
+        label any
+    }
+
+    stages {
+        stage("start test") {
+            steps {
+                build job: 'QA/TestJob',
+                        parameters: [string(name: 'threadsCount', value: threadsCount),
+                                     string(name: 'runId', value: runId),
+                                     string(name: 'testList', value: testList)]
+            }
+        }
+    }
+}
